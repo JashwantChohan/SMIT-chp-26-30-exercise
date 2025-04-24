8e6c2812c5aa469e9bce085c09738e1cd056c8e6
JashwantChohan <116147134+JashwantChohan@users.noreply.github.com>
2025-04-24 15:48:00 +0500

    Q4 done

---
+++ script.js
@@ -0,0 +1,56 @@
+// 1. Write a program that takes a positive integer from user &
+// display the following in your browser.
+// a. number
+// b. round off value of the number
+// c. floor value of the number
+// d. ceil value of the numbe
+
+// let input = prompt("Enter positve integer")
+// document.write(`Number: ${input}<br>`)
+// let roundOf = Math.round(input)
+// document.write(`Round of value: ${roundOf}<br>`)
+// let floorValue = Math.floor(input)
+// document.write(`floor value of number: ${floorValue}<br>`)
+// let ceilValue = Math.ceil(input)
+// document.write(`ceil value of number: ${ceilValue}<br`)
+
+
+// 2. Write a program that takes a negative floating point
+// number from user & display the following in your browser.
+// a. number
+// b. round off value of the number
+// c. floor value of the number
+// d. ceil value of the number
+
+
+// let input = prompt("Enter negative floating integer")
+// document.write(`Number: ${input}<br>`)
+// let roundOf = Math.round(input)
+// document.write(`Round of value: ${roundOf}<br>`)
+// let floorValue = Math.floor(input)
+// document.write(`floor value of number: ${floorValue}<br>`)
+// let ceilValue = Math.ceil(input)
+// document.write(`ceil value of number: ${ceilValue}<br`)
+
+
+// 3. Write a program that displays the absolute value of a
+// number.
+// E.g. absolute value of -4 is 4 & absolute value of 5 is 5
+
+// let num = -2
+// let result = Math.abs(num)
+// console.log("the absolute value of number is:",result)
+
+// 4. Write a program that simulates a dice using random()
+// method of JS Math class. Display the value of dice in your
+// browser.:
+
+// let no = Math.floor(Math.random()* 6) + 1  
+// document.write(`Random dice Value: ${no}`)
+
+// 5. Write a program that simulates a coin toss using random()
+// method of JS Math class. Display the value of coin in your
+// browser
+
+let no = Math.floor(Math.random()* 6) + 1  
+document.write(`Random dice Value: ${no}`)
